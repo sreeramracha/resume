9af9d602884fecb024795aa66cfd05fd8c2b5b0b
Sreeram <90460404+sreeramracha@users.noreply.github.com>
2022-04-09 01:00:58 +0530

    Added initial files

---
+++ index.js
@@ -0,0 +1,22 @@
+ctr=0
+
+function myFunction(x){
+    var link = document.createElement('link'); 
+    link.rel = 'stylesheet'; 
+    link.type = 'text/css';
+    if (ctr%2===0){
+        link.href = './css/style-dark.css'; 
+        document.getElementsByTagName('HEAD')[0].appendChild(link); 
+        // document.querySelector('.whole').classList.toggle('transition');
+    }
+    else{
+        link.href= './css/style-light.css'
+        document.getElementsByTagName('HEAD')[0].appendChild(link); 
+        // document.querySelector('.whole').classList.toggle('transition');
+    }
+    x.classList.toggle("fa-sun");
+    
+    
+    ctr+=1;
+};
+
